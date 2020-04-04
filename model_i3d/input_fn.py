88a1ba4db5f4c4ce25d5b0aa51cdce9847521c9a
Maxat Alibayev <maxatalibayev@Maxats-MacBook-Pro.local>
2020-04-04 17:54:09 -0400

    initial commit

---
+++ model_i3d/input_fn.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+import numpy as np 
+import os
+from PIL import Image
+from model_i3d.data_augment import transform_data
+import random
+
+def load_image(clip_path, index, modality):
+    """
+    Loads the frame `index` at `clip_path` location.
+    RGB frame is returned as a list of a single element.
+    Flow frames are returned as a list with 2 elements.
+    """
+    filename = 'frame{:010d}.jpg'.format(index)
+    if modality == 'rgb':
+        img = Image.open(os.path.join(clip_path.format('rgb', ''), filename)).convert('RGB')
+        return [img]
+    else:
+        u_img = Image.open(os.path.join(clip_path.format('flow', 'u/'), filename)).convert('L')
+        v_img = Image.open(os.path.join(clip_path.format('flow', 'v/'), filename)).convert('L')
+        return [u_img, v_img]
+
+
+def get_frames_and_labels(data, num_frames, sampling_rate, modality, word_embedding, is_training):
+    """
+    The function returns frames as a Numpy array, a class label, and embedded word vector.
+    """
+    # Decode strings to utf-8
+    clip_path = data[0].decode("utf-8")
+    total_frames = int(data[1].decode("utf-8"))
+    label = np.int64(int(data[2].decode("utf-8")))
+    sample_index = int(data[3].decode("utf-8"))
+    modality = modality.decode("utf-8")
+    word_embedding = word_embedding.decode("utf-8")
+
+    # List that will store the loaded frames
+    frames = []
+
+    # EPIC Flow frames were sampled at 30 fps, so they have twice less frames
+    if modality != 'rgb':
+        total_frames = total_frames // 2
+        sampling_rate = (sampling_rate + 1) // 2
+
+    # Choose the first frame where to start sampling
+    if is_training:
+        interval_size = sampling_rate * (num_frames - 1) + 1
+        start = random.randint(0, max(0, total_frames - interval_size))
+        stop = start + num_frames * sampling_rate
+    else:
+        start = 0
+        stop = max(total_frames, sampling_rate * num_frames)
+    
+    # Get list of frames
+    # In case if sampling_rate is zero, we will sample uniformly
+    if sampling_rate > 0:
+        for i in range(start, stop, sampling_rate):
+            index = (i % total_frames) + 1
+            frames.extend(load_image(clip_path, index, modality))
+    else:
+        sampling_rate = total_frames // num_frames
+        for i in range(num_frames):
+            start = int(i * sampling_rate)
+            end = max(start, int((i+1) * sampling_rate) - 1)
+            index = random.randint(start, end) + 1
+            frames.extend(load_image(clip_path, index, modality))
+            
+    # Apply transofrmations
+    frames = transform_data(frames, random_crop=is_training, random_flip=is_training)
+
+    # Convert frames to numpy arrays
+    frames_np = []
+    if modality == 'rgb':
+        for frame in frames:
+            frames_np.append(np.asarray(frame))
+    else:
+        for i in range(0, len(frames), 2):
+            frame = np.stack([np.asarray(frames[i]), np.asarray(frames[i+1])], axis=2)
+            frames_np.append(frame)
+    frames = np.stack(frames_np)
+
+    # Normalize frames
+    frames = frames / 255 * 2 - 1
+    frames = np.array(frames, dtype='float32')
+
+    # HACK: will not look for word vector for RGB frames, will be done for Flow frames anyway
+    if modality != 'rgb':
+        # Get word embedding
+        if is_training:
+            words = np.load(os.path.join('data', 'word_embeddings', word_embedding + '.npy'))
+            word = words[sample_index]
+        else:
+            words = np.load(os.path.join('data', 'word_embeddings', word_embedding + '_classkeys.npy'))
+            word = words[label]
+    else:
+        word = np.zeros(int(word_embedding[-3:]))
+        
+    return frames, label, word.astype(np.float32)
+
+
+def parse_fn(data, params, is_training):
+    """
+    A wrapper function to call `get_frames_and_labels` function
+    """
+    [frames_rgb, label, word_embedding] = tf.py_func(
+        get_frames_and_labels,
+        [data, params.num_frames, params.sampling_rate, "rgb", params.word_embedding, is_training],
+        [tf.float32, tf.int64, tf.float32]
+    )
+
+    [frames_flow, label, word_embedding] = tf.py_func(
+        get_frames_and_labels,
+        [data, params.num_frames, params.sampling_rate, "flow", params.word_embedding, is_training],
+        [tf.float32, tf.int64, tf.float32]
+    )
+
+    return frames_rgb, frames_flow, label, word_embedding
+
+
+def input_fn(data_list, params, is_training):
+    """
+    Takes the dataset information and returns processed dataset
+    """
+    # Functions that will convert video path into video frames, labels, and word embeddings
+    parse_train = lambda d: parse_fn(d, params, True)
+    parse_test = lambda d: parse_fn(d, params, False)
+
+    # Make a Dataset object that will read the list of data
+    if is_training:
+        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(data_list)))
+            .shuffle(len(data_list))
+            .map(parse_train, num_parallel_calls=params.num_parallel_calls)
+            .repeat()
+            .batch(params.batch_size)
+            .prefetch(params.prefetch_train)
+        )
+        num_steps = (len(data_list) - 1) // params.batch_size + 1
+    else:
+        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(data_list)))
+            .map(parse_test, num_parallel_calls=params.num_parallel_calls)
+            .repeat()
+            .batch(1)
+            .prefetch(params.prefetch_test)
+        )
+        num_steps = len(data_list)
+    
+    # Dimensionality of word vector
+    word_dims = int(params.word_embedding[-3:])
+
+    # Create reinitializable iterator from dataset
+    iterator = dataset.make_initializable_iterator()
+    clips_rgb, clips_flow, labels, words = iterator.get_next()
+    clips_rgb.set_shape([None, None, params.frame_size, params.frame_size, 3])
+    clips_flow.set_shape([None, None, params.frame_size, params.frame_size, 2])
+    words.set_shape([None, word_dims])
+    iterator_init_op = iterator.initializer
+
+    inputs = {
+        'clips_rgb': clips_rgb, 
+        'clips_flow': clips_flow, 
+        'labels': labels, 
+        'words': words, 
+        'iterator_init_op': iterator_init_op, 
+        'num_steps' : num_steps
+    }
+
+    return inputs
---
+++ model_i3d/model_fn.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+from model_i3d.i3d import InceptionI3d
+import numpy as np 
+import os
+from model_i3d.triplet_loss import get_valid_triplets
+
+def sum_random_triplets(triplets, num_samples):
+    total_loss = 0.0
+    for query in triplets:
+        flat = query.flatten()
+        flat = flat[np.nonzero(flat)]
+        flat = np.random.choice(flat, num_samples, replace=False)
+        total_loss += np.sum(flat)
+    return np.float(total_loss)
+
+
+def model_fn(inputs, params, is_training):
+    clips_rgb = inputs['clips_rgb']
+    clips_flow = inputs['clips_flow']
+    labels = inputs['labels']
+    words = inputs['words']
+    reuse = not is_training
+
+    # Define the entire model
+    with tf.variable_scope('Model', reuse=reuse):
+        # Define the RGB model
+        with tf.variable_scope('RGB', reuse=reuse):
+            model_rgb = InceptionI3d(num_classes=params.feature_size)
+            logits_rgb, _ = model_rgb(clips_rgb, is_training=is_training, dropout_keep_prob=1.0)
+
+        # Define the Flow model
+        with tf.variable_scope('Flow', reuse=reuse):
+            model_flow = InceptionI3d(num_classes=params.feature_size)
+            logits_flow, _ = model_flow(clips_flow, is_training=is_training, dropout_keep_prob=1.0)
+
+        # Embedding step
+        logits_visual = tf.concat([logits_rgb, logits_flow], 1)
+        logits_visual = tf.math.l2_normalize(logits_visual, axis=1)
+        logits_visual = tf.layers.dense(logits_visual, params.fc1, use_bias=False, name='visual1')
+        logits_visual = tf.layers.dense(logits_visual, params.fc2, activation=tf.nn.relu, use_bias=False, name='visual2')
+        logits_visual = tf.math.l2_normalize(logits_visual, axis=1)
+
+        logits_text = tf.math.l2_normalize(words, axis=1)
+        logits_text = tf.layers.dense(logits_text, params.fc1, use_bias=False, name='text1')
+        logits_text = tf.layers.dense(logits_text, params.fc2, activation=tf.nn.relu, use_bias=False, name='text2')
+        logits_text = tf.math.l2_normalize(logits_text, axis=1)
+
+        # Names of the variables that will be tuned for first epochs
+        top_variables = ['Logits', 'visual1', 'visual2', 'text1', 'text2']
+
+
+    # valid triplet losses
+    loss_vv = get_valid_triplets(labels, logits_visual, logits_visual, params.margin, cross_modal=False)
+    loss_tt = get_valid_triplets(labels, logits_text, logits_text, params.margin, cross_modal=False)
+    loss_vt = get_valid_triplets(labels, logits_visual, logits_text, params.margin, cross_modal=True)
+    loss_tv = get_valid_triplets(labels, logits_text, logits_visual, params.margin, cross_modal=True)
+
+    # HACK: Changed this to randomly sample `params.triplets` triplets per query
+    # loss_vv = tf.reduce_sum(loss_vv)
+    # loss_tt = tf.reduce_sum(loss_tt)
+    # loss_vt = tf.reduce_sum(loss_vt)
+    # loss_tv = tf.reduce_sum(loss_tv)
+    [loss_vv] = tf.py_func(sum_random_triplets, [loss_vv, params.num_triplets], [tf.float32])
+    [loss_tt] = tf.py_func(sum_random_triplets, [loss_tt, params.num_triplets], [tf.float32])
+    [loss_vt] = tf.py_func(sum_random_triplets, [loss_vt, params.num_triplets], [tf.float32])
+    [loss_tv] = tf.py_func(sum_random_triplets, [loss_tv, params.num_triplets], [tf.float32])
+
+    total_loss = params.lambda_within * (loss_tt + loss_vv) + params.lambda_cross * (loss_vt + loss_tv)
+
+
+    # Create variable maps for RGB and Flow I3D models
+    # Store the top layers into a list
+    if is_training:
+        variable_map_rgb = {}
+        variable_map_flow = {}
+        train_vars = []
+
+        for variable in tf.global_variables():
+            var_name = variable.name.split('/')
+            if var_name[1] == 'RGB' and var_name[2] == 'inception_i3d' and var_name[3] not in top_variables:
+                variable_map_rgb[variable.name.replace(':0', '')] = variable
+            if var_name[1] == 'Flow' and var_name[2] == 'inception_i3d' and var_name[3] not in top_variables:
+                variable_map_flow[variable.name.replace(':0', '')] = variable
+            if var_name[3] in top_variables or var_name[1] in top_variables:
+                train_vars.append(variable)
+
+        lr = tf.placeholder(tf.float32)
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            train_op_initial = optimizer.minimize(total_loss, var_list=train_vars)
+            train_op = optimizer.minimize(total_loss)
+
+    # TODO: add metrics tensor and ops to update it
+
+    # MODEL SPECIFICATION
+    model_spec = inputs
+    model_spec['loss_vv'] = loss_vv
+    model_spec['loss_tt'] = loss_tt
+    model_spec['loss_vt'] = loss_vt
+    model_spec['loss_tv'] = loss_tv
+    model_spec['total_loss'] = total_loss
+
+    if is_training:
+        model_spec['train_op_initial'] = train_op_initial
+        model_spec['train_op'] = train_op
+        model_spec['variable_map_rgb'] = variable_map_rgb   
+        model_spec['variable_map_flow'] = variable_map_flow
+        model_spec['lr'] = lr
+    
+    return model_spec
+
