3758e7bd6d628335ee4c2000dde855bb1032c85e
Maxat Alibayev <maxatalibayev@VPN-156-210.vpn.usf.edu>
2020-04-11 15:49:43 -0400

    Switched to the models pre-trained on EPIC

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 import tensorflow as tf
+import numpy as np
 import os
 import argparse
 import logging
 
-# TODO: Consider other models
-from model_i3d.utils import Params
-from model_i3d.utils import set_logger
-from model_i3d.utils import data_info
-from model_i3d.input_fn import input_fn
-from model_i3d.model_fn import model_fn
-from model_i3d.training import train_model
+from model_epic.utils import Params
+from model_epic.utils import set_logger
+from model_epic.utils import data_info
+from model_epic.data_preprocessing import preprocess
+from model_epic.input_fn import input_fn
+from model_epic.model_fn import model_fn
+from model_epic.training import train_model
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model_dir', help='Path to the directory with `params.json` file')
+parser.add_argument('-p', '--preprocess', action='store_true', help='Whether to preprocess input data or not')
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -29,10 +31,18 @@ if __name__ == "__main__":
     set_logger(os.path.join(model_dir, 'train.log'))
 
     # Input data pipeline
+    # TODO: Make it an argument
+    if not os.path.isfile(os.path.join('data', 'preprocessed.npz')) or args.preprocess:
+        visual_rgb = os.path.join('model_epic', 'visual_features', 'tsn_resnet50_rgb.npy')
+        visual_flow = os.path.join('model_epic', 'visual_features', 'tsn_resnet50_flow.npy')
+        words = os.path.join('data', 'word_embeddings', '{}_train.npy'.format(params.word_embedding))
+        split = os.path.join('data', 'splits', 'train_embedding.txt')
+        preprocess(visual_rgb, visual_flow, words, split)
+
     logging.info("Creating the dataset...")
 
-    train_data = data_info('train_embedding')
-    train_inputs = input_fn(train_data, params, is_training=True)
+    train_data = np.load(os.path.join('data', 'preprocessed.npz'))
+    train_inputs = input_fn(train_data['visuals'], train_data['words'], train_data['labels'], params, is_training=True)
 
     # Define the model
     logging.info("Building the model")
