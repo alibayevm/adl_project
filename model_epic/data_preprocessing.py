5c94448171470e3fbe294df1f1c4b960c9fecc7e
Maxat Alibayev <maxatalibayev@Maxats-MacBook-Pro.local>
2020-04-12 19:01:10 -0400

    Doesn't need to save word embeddings

--- model_epic/data_preprocessing.py
+++ model_epic/data_preprocessing.py
@@ -20,24 +20,20 @@ def preprocess(rgb, flow, words, split, is_training):
     
     np.savez(os.path.join('data', filename), visuals=visuals, words=words, labels=labels)
 
-def preprocess_test(rgb, flow, words, split):
+def preprocess_test(rgb, flow, split):
     rgb = np.load(rgb)
     flow = np.load(flow)
-    class_words = np.load(words)
     split = open(split)
 
     visuals = np.stack([rgb, flow], axis=1)
     visuals = np.average(visuals, axis=(1,2))
 
     labels = []
-    words = []
 
     for line in split:
         label = int(line.strip().split(' ')[2])
         labels.append(label)
-        words.append(class_words[label])
     
     labels = np.stack(labels)
-    words = np.stack(words)
 
-    np.savez(os.path.join('data', 'preprocessed_test.npz'), visuals=visuals, words=words, labels=labels)
+    np.savez(os.path.join('data', 'preprocessed_test.npz'), visuals=visuals, labels=labels)
