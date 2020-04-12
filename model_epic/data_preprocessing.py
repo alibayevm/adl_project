12525ff7b0c772eab4a822b8d97e6eea98b01ce3
Maxat Alibayev <maxatalibayev@VPN-156-210.vpn.usf.edu>
2020-04-12 13:11:24 -0400

    Works for all splits

--- model_epic/data_preprocessing.py
+++ model_epic/data_preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-def preprocess(rgb, flow, words, split):
+def preprocess(rgb, flow, words, split, is_training):
     rgb = np.load(rgb)
     flow = np.load(flow)
     words = np.load(words)
@@ -15,5 +15,29 @@ def preprocess(rgb, flow, words, split):
         label = int(line.strip().split(' ')[2])
         labels.append(label)
     labels = np.stack(labels)
+
+    filename = 'preprocessed_train.npz' if is_training else 'preprocessed_valid.npz'
     
-    np.savez(os.path.join('data', 'preprocessed.npz'), visuals=visuals, words=words, labels=labels)
+    np.savez(os.path.join('data', filename), visuals=visuals, words=words, labels=labels)
+
+def preprocess_test(rgb, flow, words, split):
+    rgb = np.load(rgb)
+    flow = np.load(flow)
+    class_words = np.load(words)
+    split = open(split)
+
+    visuals = np.stack([rgb, flow], axis=1)
+    visuals = np.average(visuals, axis=(1,2))
+
+    labels = []
+    words = []
+
+    for line in split:
+        label = int(line.strip().split(' ')[2])
+        labels.append(label)
+        words.append(class_words[label])
+    
+    labels = np.stack(labels)
+    words = np.stack(words)
+
+    np.savez(os.path.join('data', 'preprocessed_test.npz'), visuals=visuals, words=words, labels=labels)
