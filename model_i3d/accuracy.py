3fe72e288143d42ea16b23e6dd202df123e44cd8
Maxat Alibayev <maxatalibayev@VPN-156-108.vpn.usf.edu>
2020-04-21 18:54:15 -0400

    Loss & accuracy

---
+++ model_i3d/accuracy.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+def compute_predictions(visual_embedding, class_embeddings, labels):
+    visual_squared = tf.matmul(visual_embedding, tf.transpose(visual_embedding))
+    visual_squared = tf.diag_part(visual_squared)
+
+    class_squared = tf.matmul(class_embeddings, tf.transpose(class_embeddings))
+    class_squared = tf.diag_part(class_squared)
+
+    dot_product = tf.matmul(visual_embedding, tf.transpose(class_embeddings))
+
+    distances = tf.expand_dims(visual_squared, 1) - 2.0 * dot_product + tf.expand_dims(class_squared, 0)
+    distances = tf.sqrt(distances)
+
+    predictions = tf.argmin(distances, axis=1)
+
+    return predictions
+
+def compute_predictions_similarity(visual_embedding, class_embeddings, labels):
+    dot_product = tf.matmul(visual_embedding, tf.transpose(class_embeddings))
+    similarity = 1.0 / (1.0 + tf.exp(0.0 - dot_product))
+
+    predictions = tf.argmax(similarity, axis=1)
+
+    return predictions
